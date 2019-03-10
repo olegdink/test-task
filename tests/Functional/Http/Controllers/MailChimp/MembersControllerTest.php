babc83643e66461b2a369106ed210cf91326737a
Oleg <web@dink.ru>
2019-03-10 14:24:00 +0500

    testCreateMemberSuccessfully init working version

---
+++ tests/Functional/Http/Controllers/MailChimp/MembersControllerTest.php
@@ -0,0 +1,94 @@
+<?php
+declare(strict_types=1);
+
+
+use App\Database\Entities\MailChimp\MailChimpList;
+use App\Database\Entities\MailChimp\MailChimpMember;
+use Tests\App\TestCases\WithDatabaseTestCase;
+
+
+class MembersControllerTest extends WithDatabaseTestCase
+{
+
+    /**
+     * @param array $data
+     *
+     * @return MailChimpList
+     */
+    protected function createList(array $data): MailChimpList
+    {
+        $list = new MailChimpList($data);
+
+        $this->entityManager->persist($list);
+        $this->entityManager->flush();
+
+        return $list;
+    }
+
+    /**
+     * @param array $data
+     *
+     * @return MailChimpMember
+     */
+    protected function createMember(array $data): MailChimpMember
+    {
+        $member = new MailChimpMember($data);
+
+        $this->entityManager->persist($member);
+        $this->entityManager->flush();
+
+        return $member;
+    }
+
+    public function testCreateMemberSuccessfully(): void
+    {
+        // Create List
+
+        $listData = [
+            'name' => '2New list for new member',
+            'permission_reminder' => 'You signed up for updates on Greeks economy.',
+            'email_type_option' => false,
+            'contact' => [
+                'company' => 'New member Ltd.',
+                'address1' => 'Member DoeStreet 1',
+                'address2' => '',
+                'city' => 'Doesy',
+                'state' => 'Doedoe',
+                'zip' => '1672-12',
+                'country' => 'US',
+                'phone' => '55533344412'
+            ],
+            'campaign_defaults' => [
+                'from_name' => 'Dink rink',
+                'from_email' => 'john@doe.com',
+                'subject' => 'My new campaign!',
+                'language' => 'US'
+            ],
+            'visibility' => 'prv',
+            'use_archive_bar' => false,
+            'notify_on_subscribe' => 'notify@loyaltycorp.com.au',
+            'notify_on_unsubscribe' => 'notify@loyaltycorp.com.au'
+        ];
+
+        $this->post('/mailchimp/lists/', $listData);
+        $content = \json_decode($this->response->content(), true);
+        $this->assertResponseOk();
+
+        $lists = $this->entityManager->getRepository(MailChimpList::class)->findAll();
+        var_dump($lists[0]->getId() . ' | ' . $lists[0]->getMailChimpId());
+
+        // Create Member
+
+        $this->post('/mailchimp/lists/'.$lists[0]->getId().'/members', [
+            "email_address" => "karapuz@dxn.ru",
+        	"status" => "subscribed"
+        ]);
+        $content = \json_decode($this->response->content(), true);
+        $this->assertResponseOk();
+
+        $members = $this->entityManager->getRepository(MailChimpMember::class)->findAll();
+        var_dump($members[0]->getId() . ' | ' . $members[0]->getMailChimpId());
+    }
+
+
+}
